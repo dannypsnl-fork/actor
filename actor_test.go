7c82dfed5bd38f6b0d43ec1634253690aaac904c
dannypsnl <dannypsnl@gmail.com>
2018-05-03 17:37:14 +0800

    refactor: move Spawn to actor.go

--- actor_test.go
+++ actor_test.go
@@ -23,35 +23,6 @@ func (s *SayHello) Do(i int) {
 	close(s.receive)
 }
 
-func Spawn(actor Actorable, startArgs []interface{}) chan interface{} {
-	act := reflect.ValueOf(actor).MethodByName("Do")
-	var buf bytes.Buffer
-	for _, v := range startArgs {
-		t := reflect.TypeOf(v)
-		buf.WriteString(t.Name())
-	}
-	var buf2 bytes.Buffer
-	for i := 0; i < act.Type().NumIn(); i++ {
-		t := act.Type().In(i)
-		buf2.WriteString(t.Name())
-	}
-	expected := buf2.String()
-	input := buf.String()
-	if expected != input {
-		panic(fmt.Sprintf("expected: %s, but receive: %s", expected, input))
-	}
-	if act.Type().NumOut() > 0 {
-		panic("expected no return!!!")
-	}
-
-	inputs := make([]reflect.Value, len(startArgs))
-	for k, in := range startArgs {
-		inputs[k] = reflect.ValueOf(in)
-	}
-	go act.Call(inputs)
-	return actor.Recv()
-}
-
 func TestSpawn(t *testing.T) {
 	sayHi := &SayHello{}
 	pid := Spawn(sayHi, []interface{}{3})
